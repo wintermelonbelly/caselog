a54cb1e4b46074cc7ca48d654631350bda0ed804
wintermelonbelly <150236347+wintermelonbelly@users.noreply.github.com>
2026-04-13 16:23:19 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'caselog-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('缓存资源中...');
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request).then(function(fetchRes) {
+        return caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, fetchRes.clone());
+          return fetchRes;
+        });
+      });
+    }).catch(function() {
+      if (event.request.destination === 'document') {
+        return caches.match('./index.html');
+      }
+    })
+  );
+});
